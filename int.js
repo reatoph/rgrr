cd6a8603cd9936cf5d09969027633746a5b596b5
reatoph <reatoph@gmail.com>
2026-04-16 15:11:57 -0500

    Delete int.js

--- int.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const cards = document.querySelectorAll('.link-card');
-    cards.forEach((card, index) => {
-        setTimeout(() => {
-            card.style.opacity = "1";
-            card.style.transform = "translateY(0)";
-        }, index * 100);
-    });
-});
